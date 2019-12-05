99fcab7b5d9b37ed031ed8d6f8ea630b9a61f614
Sunil KS <kslvsunil@gmail.com>
2019-12-05 20:15:34 +0530

    adding styles

--- src/todo_app/todo.cljs
+++ src/todo_app/todo.cljs
@@ -1,14 +1,13 @@
 (ns todo-app.todo
- (:require [reagent.core :as r]))
+ (:require [reagent.core :as r]
+           [garden.core :refer [css]]))
 
 (defonce todos (r/atom (sorted-map)))
 (defonce counter (r/atom 0))
-;
+
 (defn add-todo [text]
   (let [id (swap! counter inc)]
-    (swap! todos assoc id {:id id :text text :done false :notes "hello" :due-date ""})))
-
-(defn toggle-todo [id] (swap! todos update-in [id :done] not))
+    (swap! todos assoc id {:id id :text text :done false :notes "" :due-date ""})))
 
 (defn toggle-all []
   [:button "Toggle All"])
@@ -16,9 +15,9 @@
 (defn todo-input []
   (let [input (r/atom "")]
     (fn []
-      [:form
-       [:input {:type "text" :value @input :on-change #(reset! input (-> % .-target .-value))}] 
-       [:button {:on-click (fn [e] (.preventDefault e) (add-todo @input) (reset! input ""))} "Add"]])))
+      [:form.input-form
+       [:input.todo-input {:type "text" :value @input :on-change #(reset! input (-> % .-target .-value))}] 
+       [:button.add-btn {:on-click (fn [e] (.preventDefault e) (add-todo @input) (reset! input ""))} [:i.fas.fa-plus]  " ADD"]])))
 
 (defn todo-item [{:keys [id text done notes due-date]}]
   (let [is-editing (r/atom false)
@@ -30,39 +29,49 @@
     (fn [{:keys [id text done notes due-date]}]
       [:div {:id (.toString id)}
        (if-not @is-editing
-         [:<>
-          [:button {:on-click (fn [e] (.preventDefault e) (toggle-todo id))} (if done "@  " "X  ")]
-          [:span text]
-          [:span due-date]
-          [:button {:on-click (fn [e] (reset! is-editing true))} "Edit"]
-          [:button {:on-click (fn [e] (swap! todos dissoc id))} "Delete"]
-          [:button {:on-click (fn [e] (reset! show-date-input true))} "Due Date"]
-          [:button {:on-click (fn [e] (reset! show-notes-input true))} "Notes"]
-          (when @show-date-input [:form 
-                                  [:input {:type "date" :value @edit-date :on-change #(reset! edit-date (-> % .-target .-value))}]
-                                  [:button {:on-click (fn [e] 
-                                                        (.preventDefault e)
-                                                        (swap! todos assoc-in [id :due-date] @edit-date)
-                                                        (reset! show-date-input false))} "OK"]])
-          (when @show-notes-input [:form
-                                  [:textarea {:on-change #(reset! edit-notes (-> % .-target .-value))} @edit-notes]
-                                  [:button {:on-click (fn [e]
-                                                        (.preventDefault e)
-                                                        (swap! todos assoc-in [id :notes] @edit-notes)
-                                                        (reset! show-notes-input false))} "OK"]])]
-         [:<>
+         [:div.todo-item
+          [:button.td-itm-check-btn {:on-click (fn [e]
+                                                 (.preventDefault e)
+                                                 (swap! todos update-in [id :done] not))}
+           (if done [:i.far.fa-check-circle] [:i.far.fa-circle])]
+          [:span.td-itm-text text]
+          [:div.td-itm-actions
+           [:button.act-btn {:on-click (fn [e] (reset! show-date-input true))}
+            [:i.far.fa-clock]
+            (when (not-empty due-date) [:span.td-itm-due-date due-date])]
+           [:button.act-btn {:on-click (fn [e] (reset! show-notes-input true))} [:i.far.fa-sticky-note]]
+           [:button.act-btn {:on-click (fn [e] (reset! is-editing true))} [:i.far.fa-edit]]
+           [:button.act-btn {:on-click (fn [e] (swap! todos dissoc id))} [:i.far.fa-trash-alt]]]
+          (when @show-date-input
+            [:form
+             [:input {:type "date" :value @edit-date :on-change #(reset! edit-date (-> % .-target .-value))}]
+             [:button {:on-click (fn [e]
+                                   (.preventDefault e)
+                                   (swap! todos assoc-in [id :due-date] @edit-date)
+                                   (reset! show-date-input false))} [:i.fas.fa-check]]
+             [:button {:on-click (fn [e] (.preventDefault e) (reset! show-date-input false))} [:i.fas.fa-times]]])
+          (when @show-notes-input
+            [:form
+             [:textarea {:on-change #(reset! edit-notes (-> % .-target .-value))} @edit-notes]
+             [:button {:on-click (fn [e]
+                                   (.preventDefault e)
+                                   (swap! todos assoc-in [id :notes] @edit-notes)
+                                   (reset! show-notes-input false))} [:i.fas.fa-check]]
+             [:button {:on-click (fn [e] (.preventDefault e) (reset! show-notes-input false))} [:i.fas.fa-times]]])]
+         [:div.todo-item-editing
           [:form
            [:input {:type "text" :value @edit-input :on-change #(reset! edit-input (-> % .-target .-value))}]
            [:button {:on-click (fn [e]
                                  (.preventDefault e)
                                  (reset! is-editing false)
-                                 (swap! todos assoc-in [id :text] @edit-input))} "OK"]
-           [:button {:on-click (fn [e] (.preventDefault e) (reset! is-editing false))} "Cancel"]]])])))
+                                 (swap! todos assoc-in [id :text] @edit-input))} [:i.fas.fa-check]]
+           [:button {:on-click (fn [e] (.preventDefault e) (reset! is-editing false))} [:i.fas.fa-times]]]])])))
 
 (defn todo-app []
-  [:div
+  [:div.app
+   [:h2#title "Todo App"]
    [todo-input]
    (for [todo (vals @todos)] ^{:key (:id todo)} [todo-item todo])])
 
 (defn mount-root []
-  (r/render [todo-app] (.getElementById js/document "app")))
+  (r/render [todo-app] js/document.body))
