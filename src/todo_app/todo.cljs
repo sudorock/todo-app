6d7ca6c97139f9cf484958b3e0a61b8444ad2e20
Sunil KS <kslvsunil@gmail.com>
2019-12-05 08:53:26 +0530

    add and delete working

--- src/todo_app/todo.cljs
+++ src/todo_app/todo.cljs
@@ -1,35 +1,37 @@
 (ns todo-app.todo
  (:require [reagent.core :as r]))
 
-;(defonce todos (r/atom (sorted-map)))
-;(defonce counter (r/atom 0))
+(defonce todos (r/atom (sorted-map)))
+(defonce counter (r/atom 0))
 ;
-;(defn add-todo [text]
-;      (let [id (swap! counter inc)]
-;           (swap! todos assoc id {:id id :text text :done false})))
+(defn add-todo [text]
+  (let [id (swap! counter inc)]
+    (swap! todos assoc id {:id id :text text :done false})))
 ;
 ;(defn toggle [id] (swap! todos update-in [id :done] not))
 ;
-;(defn todo-input []
-;      [:input {:type "text"}])
-;
-;(defn todo-item [{:keys [id text done]}])
-;
-;
-;
-;(defn todo-app []
-;      [:div
-;       [todo-input]])
+(defn todo-input []
+  (let [input (r/atom "")]
+    (fn []
+      [:form
+       [:input {:type "text" :value @input :on-change #(reset! input (-> % .-target .-value))}] 
+       [:button {:on-click (fn [e] (.preventDefault e) (add-todo @input) (reset! input ""))} "Add"]])))
 
-(defn counting-button [txt]
-  (let [state (r/atom 0)]
-    (fn [txt]
-      [:button.green {:on-click #(swap! state inc)} (str txt " " @state)])))
+(defn todo-item [{:keys [id text done]}]
+(let [editing (r/atom false)]
+  [:div {:id (.toString id)}
+   [:span (if done "@  " "X  ")]
+   [:span text]
+   [:button {:on-click (fn [e] ())} "Edit"]
+   [:button {:on-click (fn [e] (swap! todos dissoc id))} "Delete"]]))
+
+(defn todo-app []
+  [:div
+   [todo-input]
+   (for [todo (vals @todos)] ^{:key (:id todo)} [todo-item todo])])
+
+(defn app []
+  [:div [todo-input]]) 
 
 (defn mount-root []
-  (r/render [counting-button] (.getElementById js/document "app")))
-
-(mount-root)
-
-(defn init []
-  (js/console.log "Hello World Sunil"))
+  (r/render [todo-app] (.getElementById js/document "app")))
